8fcc202defa73214e3eaab5200bf3d04c0c149b3
MateoLCS <mateusz.namysl0318@gmail.com>
2023-08-15 23:05:13 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1 className="text-red-500 text-4xl">React App</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
